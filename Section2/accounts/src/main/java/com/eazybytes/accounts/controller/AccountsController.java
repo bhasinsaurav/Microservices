9e1184c08439826f0207a26c310d97e10fcc5c2b
Saurav_Bhasin <bhasinsaurav9@gmail.com>
2025-07-21 22:15:10 -0400

    Started Learning about microservices

---
+++ Section2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AccountsController {
+
+
+}
