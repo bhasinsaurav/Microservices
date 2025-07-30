fd692d8f3aa8211efdb56f176fd2b581f13040d0
Saurav Bhasin <sauravbhasin@Sauravs-MacBook-Air.local>
2025-07-29 20:11:37 -0400

    Dtos added

---
+++ Section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private String errorTime;
+}
