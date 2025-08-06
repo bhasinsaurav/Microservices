809f8fa6fe4a42455d037ec8e97352b249e9bc1a
Saurav Bhasin <sauravbhasin@Sauravs-MacBook-Air.local>
2025-08-06 17:12:02 -0400

    Merge conflict resolved

--- Section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ Section2/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
