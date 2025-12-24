38dfb414959c8b6a0e9c13835890e56db0755dbe
Saurav Bhasin <bhasinsaurav9@gmail.com>
2025-12-24 17:30:47 -0500

    Gateway Server Added to the EzzyBank

---
+++ Section8/accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -0,0 +1,55 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.service.ICustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Tag(
+        name = "REST API for customers",
+        description = "REST API to fetch customer details"
+)
+@RequestMapping(path= "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    private final ICustomerService iCustomerService;
+
+    public CustomerController(ICustomerService iCustomerService) {
+        this.iCustomerService = iCustomerService;
+
+    }
+
+
+    @Operation(
+            summary = "Fetch Account Rest API",
+            description = "REST API to fetch a customer and an account for a customer"
+    )
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status Ok - Account fetched successfully"
+    )
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails( @RequestHeader("eazybank-correlation-id") String correlationId,
+                                                                    @RequestParam
+                                                                   @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                                   String mobileNumber){
+        logger.debug("eazyBank-correlation_id found: {}", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber, correlationId);
+        return new ResponseEntity<>(customerDetailsDto, HttpStatus.OK);
+    }
+}
